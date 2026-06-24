2075d9ed6aecd3af4da388a9d9f44cf0c1e1c5b9
dyedysJuina <dyedysjuina@gmail.com>
2026-06-24 15:36:08 -0400

    fix: fulltext migration handle duplicate

--- database/migrations/2026_06_24_170000_add_fulltext_to_produto_variacoes.php
+++ database/migrations/2026_06_24_170000_add_fulltext_to_produto_variacoes.php
@@ -7,7 +7,11 @@ return new class extends Migration
 {
     public function up(): void
     {
-        DB::statement('ALTER TABLE produto_variacoes ADD FULLTEXT INDEX produto_variacoes_nome_completo_fulltext (nome_completo)');
+        try {
+            DB::statement('ALTER TABLE produto_variacoes ADD FULLTEXT INDEX produto_variacoes_nome_completo_fulltext (nome_completo)');
+        } catch (\Exception $e) {
+            if (!str_contains($e->getMessage(), '1061')) throw $e;
+        }
     }
 
     public function down(): void
